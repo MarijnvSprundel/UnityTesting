21cf24e1d821641190f41d01e8d0fb0413431ae3
MarijnvSprundel <71122319+MarijnvSprundel@users.noreply.github.com>
2021-12-15 12:43:15 +0100

    Even better bombs, with throwing

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -7,28 +7,54 @@ public class Bomb : MonoBehaviour
 {
     public float force;
     public bool affectsPlayers;
-    
+    private bool explodedOthers = false;
+
+    private void Start()
+    {
+        StartCoroutine(ExampleCoroutine());
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+        Explode(10);
+    }
+
     public void Explode(float radius)
     {
-        Vector3 position = transform.position;
-        Collider[] hitColliders = Physics.OverlapSphere(position, radius);
-        foreach(var hitCollider in hitColliders)
+        if (!explodedOthers)
         {
-            if (hitCollider.GetComponent<Rigidbody>())
+            Vector3 position = transform.position;
+            Collider[] hitColliders = Physics.OverlapSphere(position, radius);
+            foreach(var hitCollider in hitColliders)
             {
-                Vector3 hitPos = hitCollider.GetComponent<Rigidbody>().position;
-                float distance = Distance(hitPos, position);
-                hitCollider.GetComponent<Rigidbody>().velocity = ((radius - distance) * (hitCollider.GetComponent<Rigidbody>().position - position)) * (force * 0.1F);
+                if (hitCollider.CompareTag("Bomb") && hitCollider.gameObject != gameObject)
+                {
+                    explodedOthers = true;
+                    hitCollider.GetComponent<Bomb>().Explode(10);
+                }
+            
+                if (hitCollider.GetComponent<Rigidbody>())
+                {
+                    Vector3 hitPos = hitCollider.GetComponent<Rigidbody>().position;
+                    float distance = Distance(hitPos, position);
+                    hitCollider.GetComponent<Rigidbody>().velocity = ((radius - distance) * (hitCollider.GetComponent<Rigidbody>().position - position)) * (force * 0.1F);
+                }
+                else if (hitCollider.GetComponent(typeof(PlayerController)) && affectsPlayers)
+                {
+                    PlayerController playerController = (PlayerController) hitCollider.GetComponent(typeof(PlayerController));
+                    Vector3 hitPos = playerController.transform.position;
+                    float distance = Distance(hitPos, position);
+                    playerController.AddImpact(((radius - distance) * (hitCollider.GetComponent(typeof(PlayerController)).transform.position - position)) * (force * 0.1F));
+                }
             }
-            else if (hitCollider.GetComponent(typeof(PlayerController)) && affectsPlayers)
-            {
-                PlayerController playerController = (PlayerController) hitCollider.GetComponent(typeof(PlayerController));
-                Vector3 hitPos = playerController.transform.position;
-                float distance = Distance(hitPos, position);
-                playerController.AddImpact(((radius - distance) * (hitCollider.GetComponent(typeof(PlayerController)).transform.position - position)) * (force * 0.1F));
-            }
+            GameObject go = Instantiate((GameObject)Resources.Load("Prefabs/Explosion", typeof(GameObject)), position, Quaternion.identity);
+            go.GetComponent<ParticleSystem>().Play();
+            Destroy(gameObject);
+            Destroy(go, 2f);
+
         }
-        Destroy(gameObject);
+        
     }
 
     float Distance(Vector3 pos1, Vector3 pos2)
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -41,39 +42,34 @@ public class PlayerController : MonoBehaviour
         // impactForce = Vector3.Lerp(impactForce, Vector3.zero, 5000*Time.deltaTime);
         impactForce /= 200F;
         
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
-            {
-                if(hit.transform.gameObject.GetComponent(typeof(Bomb)))
-                {
-                    Bomb bomb = (Bomb) hit.transform.gameObject.GetComponent(typeof(Bomb));
-                    bomb.Explode(10);
-                }
-            }
-        }
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     RaycastHit hit;
+        //     Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        //     if (Physics.Raycast(ray, out hit))
+        //     {
+        //         if(hit.transform.gameObject.GetComponent(typeof(Bomb)))
+        //         {
+        //             Bomb bomb = (Bomb) hit.transform.gameObject.GetComponent(typeof(Bomb));
+        //             bomb.Explode(10);
+        //         }
+        //     }
+        // }
         if (Input.GetKeyDown("e"))
         {
-            RaycastHit hit;
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
-            {
-                    
-                GameObject bomb = Instantiate(bombPrefab, hit.point + new Vector3(0, 0.3F, 0), Quaternion.identity);
-            }
+            GameObject bomb = Instantiate(bombPrefab, cam.transform.position + cam.transform.forward, Quaternion.identity);
+            bomb.GetComponent<Rigidbody>().velocity = cam.transform.forward * 20;
         }
 
-        if (Input.GetKeyDown("x"))
-        {
-            GameObject[] bombObjects = GameObject.FindGameObjectsWithTag("Bomb");
-            foreach (GameObject bombObject in bombObjects)
-            {
-                Bomb bomb = (Bomb) bombObject.GetComponent(typeof(Bomb));
-                bomb.Explode(10);
-            }
-        }
+        // if (Input.GetKeyDown("x"))
+        // {
+        //     GameObject[] bombObjects = GameObject.FindGameObjectsWithTag("Bomb");
+        //     foreach (GameObject bombObject in bombObjects)
+        //     {
+        //         Bomb bomb = (Bomb) bombObject.GetComponent(typeof(Bomb));
+        //         bomb.Explode(10);
+        //     }
+        // }
     }
     public void AddImpact(Vector3 force)
     {
